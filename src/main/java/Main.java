d10b8159643e4ec9a68bf22476857ef207f292a3
Julian Benitez <jubenitez@itba.edu.ar>
2018-11-01 13:34:14 -0300

    changed thing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,15 +20,15 @@ public class Main {
     public static void main(String[] args) {
         Integer iterations = 6;
         Random random = new Random();
-//        List<List<Double>> escapeTimesList = new ArrayList<>();
-//        for (int i = 0; i < iterations; i++) {
-//            escapeTimesList.add(simulate(1.0+i*0.5,maxPeopleCount, random));
-//        }
-//        System.out.println(OctaveBuilder.plot(escapeTimesList));
-//        System.out.println(OctaveBuilder.plotMean(escapeTimesList));
-//        System.out.println(OctaveBuilder.plotMean(escapeTimesList));
-
-        simulateSpeeds(1.0,3.0,0.5,3,random);
+        List<List<Double>> escapeTimesList = new ArrayList<>();
+        for (int i = 0; i < iterations; i++) {
+            escapeTimesList.add(simulate(1.0+i*0.5,maxPeopleCount, random));
+        }
+        System.out.println(OctaveBuilder.plot(escapeTimesList));
+        System.out.println(OctaveBuilder.plotMean(escapeTimesList));
+        System.out.println(OctaveBuilder.plotMean(escapeTimesList));
+//
+//        simulateSpeeds(1.0,3.0,0.5,3,random);
     }
 
     private static void simulateSpeeds(Double start, Double end, Double inteval, Integer iterations, Random random){
